bd8751c33ee2713d5f7c36bf06dd7cc2c0567b62
aleenalwilson <78653555+aleenalwilson@users.noreply.github.com>
2022-11-19 12:19:21 +0530

    Update streamlit_app.py
    Added pandas library to display the csv file from s3 bucket

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,14 @@
 import streamlit
+import pandas
 
 streamlit.title("My Parents New Healthy Diner")
-streamlit.header('Breakfast Menu')
-streamlit.header("🥣 🥗 🐔 🥑🍞")
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Favorites')
 
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
